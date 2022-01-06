91469b4b7493c56421bfeaac3cb22dd349f6b246
DerDavidBohl <david@bohl24.de>
2022-01-06 01:43:24 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+console.log('Launching app...')
+
+const app = express()
+const port = 8080
+
+app.get('/hello-world', (req, res) => {
+    res.send({message: 'Hello World!'})
+})
+
+app.listen(port, () => {
+    console.log(`Application started and listening to port ${port}`)
+})
